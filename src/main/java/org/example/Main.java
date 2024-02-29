dbb891761ced61b7b3f027821c070312f5e23147
zoey <pegarside@hotmail.co.uk>
2024-02-29 13:11:22 +0000

    Grid draw is functional and creates grid of correct size.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -11,8 +11,8 @@ import java.util.ArrayList;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    int gridXMax = 2;
-    int gridYMax = 2;
+    int gridXMax = 1;
+    int gridYMax = 1;
     char mine = '¤';
     char flag = '¶';
     char empty = '░';
@@ -24,8 +24,8 @@ public class Main {
 
 
     public static void main(String[] args) {// Main running loop, make this run good
-        int gridXMax = 2;
-        int gridYMax = 2;
+        int gridXMax = 4;
+        int gridYMax = 5;
         // These chars are effectively the sprites for the game grid
         char mine = '¤';
         char flag = '¶';
@@ -33,9 +33,9 @@ public class Main {
         char undug = '█';
         char flagWrong = '╔';
         //TODO: A list called Grid that is made of tiles between 0 and gridMax should be built
-        ArrayList<ArrayList<Tile>> grid = constructGrid(gridXMax, gridYMax);
-        System.out.println(grid);
+        ArrayList<ArrayList<Tile>> grid = constructGrid(gridXMax , gridYMax);
         printGrid(grid);
+        //System.out.println(grid.get(1).get(1).displayChar);
     }
 
     public static ArrayList<ArrayList<Tile>> constructGrid(int gridXMax, int gridYMax) {
@@ -44,10 +44,11 @@ public class Main {
         ArrayList<Tile> row = new ArrayList<>();
 
         for (int y = 0; y < gridYMax; y++) {
-            for (int x = 0; x < gridYMax; x++) {
+            for (int x = 0; x < gridXMax; x++) {
                 row.add(new Tile(x,y, gridXMax, gridYMax));
             }
             grid.add(row);
+            row = new ArrayList<>();
         }
         return grid;
     }
@@ -58,7 +59,6 @@ public class Main {
         for (ArrayList<Tile> Row : grid) {
             for (Tile tile : Row) {
                 buffer += tile.displayChar;
-                ;
             }
             System.out.println(buffer);
             buffer = "";
@@ -70,5 +70,77 @@ public class Main {
             char displayChar = '█';
         }
     }*/
+    public int getAdjBomb(Tile tile) { //
+        int posX = tile.posX;
+        int posY = tile.posY;
 
+        int xMin = getAdjMin(posX);
+        int xMax = getAdjMax(posX, gridXMax);
+        int yMin = getAdjMin(posY);
+        int yMax = getAdjMax(posY, gridYMax);
+        int bombCount = 0;
+        for (int x = xMin; x < xMax; x++) {//TODO: Check this arithmatic
+            for (int y = yMin; y < yMax; y++) {
+                if (grid.get(x).get(y).hasBomb) {
+                    bombCount++;
+                }
+            }
+        }
+        return bombCount;
+    }
+    private static int getAdjMin(int position){
+        if (position == 0) {return 0;}
+        else {return position-1;}
+    }
+    public static int getAdjMax(int position, int gridVMax){
+        if (position == gridVMax) { return gridVMax;}
+        else {return position+1;}
+    }
+
+    public void reveal(Tile tile){
+        //TODO this is for a reveal where
+        int posX = tile.posX;
+        int posY = tile.posY;
+        ArrayList<Tile> strip = new ArrayList<>();
+
+        int xMin = getAdjMin(posX);
+        int xMax = getAdjMax(posX, gridXMax);
+        int yMin = getAdjMin(posY);
+        int yMax = getAdjMax(posY, gridYMax);
+        for (int x = xMin; x < xMax; x++) {//TODO: Check this arithmatic
+            for (int y = yMin; y<yMax; y++){
+                strip = grid.get(y);
+                strip.set(x,dig(tile));
+            }
+        }
+
+    }
+    public Tile dig(Tile tile) {
+
+        char empty = '░';
+        if (!tile.isDug) {
+            if (!tile.hasFlag) {
+                tile.isDug = true;
+                if (tile.hasBomb) {
+                    System.out.println("Game over");
+                    // TODO: make this end the game
+                } else {
+                    int adjBomb = getAdjBomb(tile);
+                    if (adjBomb == 0) {
+                        reveal(tile);
+                        tile.displayChar = empty; //
+                    }
+                    else{
+                        tile.displayChar = '0';
+                        tile.displayChar += adjBomb;
+                    }
+                }
+            }
+            System.out.println("Can't dig flag!");
+        }
+        System.out.println("Tile Already Dug");
+
+        return tile;
+
+    }
 }
--- src/main/java/org/example/Tile.java
+++ src/main/java/org/example/Tile.java
@@ -3,6 +3,11 @@ package org.example;
 import java.util.ArrayList;
 
 public class Tile {
+    char mine = '¤';
+    char flag = '¶';
+    char empty = '░';
+    char undug = '█';
+    char flagWrong = '╔';
 
     char displayChar = '█';
     boolean hasBomb = false;
@@ -16,85 +21,38 @@ public class Tile {
     int gridYMax = 0;
 
 
-    Tile(int x, int y, int gridXMax, int gridYMax) {
-        char displayChar = '█';
-        boolean hasBomb = false;
-        boolean isDug = false;
-        boolean hasFlag = false;
-        boolean isFlaggedCorrect = (hasBomb == hasFlag);
-        this.gridXMax = gridXMax;
-        this.gridYMax = gridYMax;
-        int posX = x;
-        int posY = y;
-        int adjBomb = 0;
+    Tile(int x, int y, int gridXMaxx, int gridYMaxx) {
+        displayChar = undug;
+        hasBomb = false;
+        isDug = false;
+        hasFlag = false;
+        isFlaggedCorrect = (hasBomb == hasFlag);
+        gridXMax = gridXMaxx;
+        gridYMax = gridYMaxx;
+        posX = x;
+        posY = y;
+        adjBomb = 0;
     }
 
-
-
-    public static void main(String[] args){
-    }
-    public void dig() {
-        char empty = '░';
+    public void toggleFlag() {
         if (!isDug) {
-            if (!hasFlag) {
-                isDug = true;
-                if (hasBomb) {
-                    System.out.println("Game over");
-                    // TODO: make this end the game
-                } else {
-                    adjBomb = getAdjBomb();
-                    if (adjBomb == 0) {
-                        reveal();
-                        displayChar = empty; //
-                    }
-                    else{
-                        displayChar = '0';
-                        displayChar += adjBomb;
-                    }
-                }
-            }
-            System.out.println("Can't dig flag!");
-        }
-        System.out.println("Tile Already Dug");
-
-    }
-    private static int getAdjMin(int x){
-        if (x == 0) {return 0;}
-        else {return x-1;}
-    }
-    public static int getAdjMax(int position, int gridVMax){
-        if (position == gridVMax) { return gridVMax;}
-        else {return position+1;}
-    }
-    public int getAdjBomb(ArrayList<ArrayList<Tile>> grid) { //
-        int xMin = getAdjMin(posX);
-        int xMax = getAdjMax(posX, gridXMax);
-        int yMin = getAdjMin(posY);
-        int yMax = getAdjMax(posY, gridYMax);
-        int bombCount = 0;
-        for (int x = xMin; x < xMax; x++) {//TODO: Check this arithmatic
-            for (int y = yMin; y < yMax; y++) {
-                if (grid.get(x).get(y).hasBomb) {
-                    bombCount++;
-                }
+            if (hasFlag) {
+                hasFlag = false;
+                displayChar = undug;
+            } else {
+                hasFlag = true;
+                displayChar = flag;
             }
         }
-        return bombCount;
     }
 
-    public ArrayList<ArrayList<Tile>> reveal(ArrayList<ArrayList<Tile>> grid){
-        //TODO this is for a reveal where
-        int xMin = getAdjMin(posX);
-        int xMax = getAdjMax(posX, gridXMax);
-        int yMin = getAdjMin(posY);
-        int yMax = getAdjMax(posY, gridYMax);
-        for (int x = xMin; x < xMax; x++) {//TODO: Check this arithmatic
-            for (int y = yMin; y<yMax; y++){
-                grid.get(x).get(y).dig();
-            }
+    public void checkFlag() {
+        if (hasFlag) {
+            isFlaggedCorrect = hasBomb;
         }
-        return grid;
-
     }
 
+
+    public static void main(String[] args) {
+    }
 }
